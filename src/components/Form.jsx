dddfa206ee08d06f676005a872f2274b7070b6e3
romuald gaboriau <gaboriau.r@gmail.com>
2021-04-28 13:35:18 -0400

    Form component created without state

---
+++ src/components/Form.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Enter your name',
+      email: 'Enter your email'
+    };
+  }
+
+  isEmailValid = (email) => {
+    if (!email.match(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/)) {
+      return alert("Please enter a valid email");
+    }
+  };
+
+
+  handleChange = (event) => {
+    // this.event.preventDefault();
+    this.setState({ name: event.target.value });
+    this.setState({ email: event.target.value });
+  }
+
+  render () {
+    return (
+      <div>
+        <form>
+          <h2>Sign up to get your free trial today!</h2>
+          <div className="form-group">
+            <input type="email" className="form-control" value={this.state.name} onChange={this.handleChange} />
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control" value={this.state.email} onChange={this.handleChange} onBlur={this.isEmailValid(this.state.email)} />
+          </div>
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" />
+          </div>
+          <button type="submit" className="btn btn-primary">Start my free trial</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
