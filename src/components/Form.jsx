f3c619b9f26da206d919bf7e5c3a38f0e68eac21
romuald gaboriau <gaboriau.r@gmail.com>
2021-04-28 16:49:01 -0400

    Added validations for Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,8 +7,8 @@ class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Enter your name',
-      email: 'Enter your email',
+      name: '',
+      email: '',
       checked: false,
       showModal: false
     };
@@ -30,16 +30,14 @@ class Form extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    if (this.isEmailValid && this.state.checked === true) {
+    if (this.isEmailValid && this.state.email !== '' && this.state.name !== '' && this.state.checked === true) {
       return this.setState({ showModal: true });
     }
     return this.state.showModal;
-    console.log(this.state.showModal);
   }
 
   handleClick = () => {
     this.setState({ checked: !this.state.checked });
-    console.log(this.state.checked);
   }
 
   hideModal = () => {
@@ -48,25 +46,40 @@ class Form extends Component {
 
   render () {
     return (
-      <div>
-        <form className="form">
-          <h2>Sign up to get your free trial today!</h2>
-          <div className="form-group">
-            <label>
-              Name
-              <input type="text" className="form-control" value={this.state.name} onChange={this.handleChangeName} />
-            </label>
-          </div>
-          <div className="form-group">
-            <input type="email" className="form-control" value={this.state.email} onChange={this.handleChangeEmail} onBlur={() => this.isEmailValid(this.state.email)} />
-          </div>
-          <div className="form-check">
-            <input type="checkbox" onClick={this.handleClick} className="form-check-input" />
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Start my free trial</button>
-        </form>
+      <div className="container">
+        <div className="left-scene col-sm-6 col-md-4">
+          <img src="../../assets/photos/upword-logo.svg" alt="" />
+          <h1>Get More From Your <span>Content</span>.</h1>
+          <p>UpWord helps you optimize your brand's publications to reach a larger audience, integrating social content with your website.</p>
+        </div>
 
-        <Modal show={this.state.showModal} handleClose={this.hideModal} />
+        <div className="col-sm-6 col-md-4">
+          <form className="form">
+          <p className="form-limited-offer">Limited offer</p>
+            <h2>Sign up to get your free trial today!</h2>
+            <div className="form-group">
+              <label>
+                Name
+                <input type="text" className="form-control" value={this.state.name} onChange={this.handleChangeName} placeholder="Enter your name" />
+              </label>
+            </div>
+            <div className="form-group">
+              <label>
+                Email
+              <input type="email" className="form-control" value={this.state.email} onChange={this.handleChangeEmail} onBlur={() => this.isEmailValid(this.state.email)} placeholder="Enter your email" />
+              </label>
+            </div>
+            <div className="form-check">
+              <label>
+              <input type="checkbox" onClick={this.handleClick} className="form-check-input" />
+              I agree to the terms and conditions
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Start my free trial</button>
+          </form>
+
+          <Modal show={this.state.showModal} handleClose={this.hideModal} name={this.state.name} email={this.state.email} />
+        </div>
       </div>
     );
   }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../../assets/stylesheets/modal.scss';
 
-const Modal = ({ handleClose, show, children }) => {
+const Modal = ({ handleClose, show, name, email }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        {children}
-        <button type="button" onClick={handleClose}>
-          Close
+        <div>
+          <img src="../../assets/photos/upword-illustration.png" alt="Welcome photo" />
+        </div>
+        <h3>Welcome {name}!</h3>
+        <p>Thanks for filling out the form. A confirmation email has been sent to {email} </p>
+
+        <button type="button" onClick={handleClose} className="modal-button">
+          X
         </button>
       </section>
     </div>
