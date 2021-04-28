00d59902b226f8359c4c0ded4e75c575739c43bb
romuald gaboriau <gaboriau.r@gmail.com>
2021-04-28 15:20:38 -0400

    Added Modal Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Modal from './Modal';
+
 
 class Form extends Component {
   constructor(props) {
@@ -7,7 +9,8 @@ class Form extends Component {
     this.state = {
       name: 'Enter your name',
       email: 'Enter your email',
-      checked: false
+      checked: false,
+      showModal: false
     };
   }
 
@@ -17,32 +20,53 @@ class Form extends Component {
     }
   };
 
-
-  handleChange = (event) => {
+  handleChangeName = (event) => {
     this.setState({ name: event.target.value });
+  }
+
+  handleChangeEmail = (event) => {
     this.setState({ email: event.target.value });
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
+    if (this.isEmailValid && this.state.checked === true) {
+      return this.setState({ showModal: true });
+    }
+    return this.state.showModal;
+    console.log(this.state.showModal);
+  }
+
+  handleClick = () => {
+    this.setState({ checked: !this.state.checked });
+    console.log(this.state.checked);
+  }
+
+  hideModal = () => {
+    return this.setState({ showModal: false });
   }
 
   render () {
     return (
       <div>
-        <form>
+        <form className="form">
           <h2>Sign up to get your free trial today!</h2>
           <div className="form-group">
-            <input type="email" className="form-control" value={this.state.name} onChange={this.handleChange} />
+            <label>
+              Name
+              <input type="text" className="form-control" value={this.state.name} onChange={this.handleChangeName} />
+            </label>
           </div>
           <div className="form-group">
-            <input type="email" className="form-control" value={this.state.email} onChange={this.handleChange} onBlur={() => this.isEmailValid(this.state.email)} />
+            <input type="email" className="form-control" value={this.state.email} onChange={this.handleChangeEmail} onBlur={() => this.isEmailValid(this.state.email)} />
           </div>
           <div className="form-check">
-            <input type="checkbox" className="form-check-input" />
+            <input type="checkbox" onClick={this.handleClick} className="form-check-input" />
           </div>
-          <button type="submit" className="btn btn-primary" onSubmit={this.handleSubmit}>Start my free trial</button>
+          <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Start my free trial</button>
         </form>
+
+        <Modal show={this.state.showModal} handleClose={this.hideModal} />
       </div>
     );
   }
